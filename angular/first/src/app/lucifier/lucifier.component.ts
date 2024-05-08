71fa795e3f912648f283044fce7e993379dd970d
rkapur68 <rkapur.2101@gmail.com>
2024-05-07 17:41:12 -0700

    First message

---
+++ angular/first/src/app/lucifier/lucifier.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lucifier',
+  templateUrl: './lucifier.component.html',
+  styleUrl: './lucifier.component.css',
+})
+export class LucifierComponent {
+  title = 'first';
+}
